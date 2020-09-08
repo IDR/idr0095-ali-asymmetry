b2676fb66a4218dde5f36d9bd14cea805fe3d966
Sebastien Besson <seb.besson@gmail.com>
2020-09-08 12:04:39 +0100

    Handle mismatching mask paths

--- scripts/upload_masks.py
+++ scripts/upload_masks.py
@@ -12,6 +12,20 @@ import sys
 NAMESPACE = 'openmicroscopy.org/idr/analysis/original'
 MIMETYPE = 'image/tiff'
 
+# Most masks can easily be associated to the corresponding nd2 file as the
+# name of the directory/file prefix matches the nd2 filename. The mapping
+# below handles the cases where there is a mismatch in the uploaded data.
+MASK_MAPPING = [
+    "AO1TO1-Trial1/HG/HG": "AO1TO1-Trial1/HG/HG105",
+    "AO1TO2-Trial1/HG/HG": "AO1TO2-Trial1/HG/HG105",
+    "AO1TOid-Trial1/HG/HG": "AO1TOid-Trial1/HG/HG105",
+    "AO1TOid-Trial2/HG105/HG105": "AO1TOid-Trial2/HG/HG",
+    "DAStag_Trial2/DAS/DAS": "DAStag_Trial2/B/B",
+    "DAStag_Trial2/DASnoO1V1/DASnoO1V1": "DAStag_Trial2/no/no",
+    "Notag_Trial2/ATnoO1V1/ATnoO1V1": "Notag_Trial2/no/no",
+    "Notag_Trial3/ATnoO1V1/ATnoO1V1": "Notag_Trial3/no/no",
+]
+
 log = logging.getLogger()
 
 
@@ -32,6 +46,11 @@ def find_masks(project):
             mask_name = "%s-%03d-Mask.tif" % (
                 os.path.basename(base), image.series + 1)
             mask_path = os.path.join(mask_folder, mask_name)
+
+            # Handle mismatching repositories
+            for source, dest in MASK_MAPPING.items():
+                mask_path = mask_path.replace(source, dest)
+
             if not os.path.exists(mask_path):
                 log.error("%s does not exist" % mask_path)
             else:
@@ -41,7 +60,7 @@ def find_masks(project):
 
 def check_unused_masks(mask_paths):
     tiff_files = set(glob.glob(
-        '/uod/idr/filesets/idr0095-ali-asymmetry/20200831-ftp/**/*.tif'))
+        '/uod/idr/filesets/idr0095-ali-asymmetry/20200831-ftp/**/**/*.tif'))
     missing_masks = tiff_files.difference(mask_paths)
     for f in missing_masks:
         log.error("%s is not associated with an image" % f)
@@ -62,12 +81,12 @@ def main(argv):
     with omero.cli.cli_login() as c:
         conn = omero.gateway.BlitzGateway(client_obj=c.get_client())
         mask_paths = []
-]        for experiment in ['experimentA', 'experimentB', 'experimentC']:
+        for experiment in ['experimentA', 'experimentB', 'experimentC']:
             project = conn.getObject(
                 'Project',
                 attributes={'name': 'idr0095-ali-asymmetry/%s' % experiment})
             log.info("Entering %s" % project.getName())
-            mask_paths.append(find_masks(project))
+            mask_paths.extend(find_masks(project))
 
     # Check all masks are associated with an image
     check_unused_masks(set(mask_paths))
